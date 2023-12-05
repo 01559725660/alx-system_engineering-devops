e797d5b6eb23625397d6ba566907945158a23eb9
Batool Khaled <122816453+01559725660@users.noreply.github.com>
2023-12-05 11:11:14 +0000

    devops

--- 2-repetition_token_1.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-#the regular expression that will match the above cases
-#Using the project instructions
-#create a Ruby script that accepts one argument
-#pass it to a regular expression matching method
-puts ARGV[0].scan(/hb?tn/).join
--- 4-repetition_token_3.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-#the regular expression that will match the above cases
-#Using the project instructions
-#create a Ruby script that accepts one argument
-#pass it to a regular expression matching method
-#Your regex should not contain square brackets
-puts ARGV[0].scan(/hbt*n/).join
